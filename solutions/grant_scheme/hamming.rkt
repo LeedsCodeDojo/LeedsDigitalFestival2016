99b803bc170919af7a86d6ed5119430113836efd
Grant Crofton <grantac@hotmail.com>
2016-04-27 10:06:48 +0100

    Refactored scheme hamming

--- solutions/grant_scheme/hamming.rkt
+++ solutions/grant_scheme/hamming.rkt
@@ -1,13 +1,16 @@
 #lang racket
 
 (define (distance string1 string2)
+
   (if (not (= (string-length string1) (string-length string2)))
       "Error"
-      (let ((chars1 (string->list string1)) (chars2 (string->list string2))) ; convert strings to list of chars
-        (cond
-          ((and (null? chars1) (null? chars2)) 0) ; both lists empty? we're done
-          (else 
-           (let ((countDifferentChar (if (equal? (first chars1) (first chars2)) 0 1)))
-             (+ countDifferentChar (distance (list->string (rest chars1)) (list->string (rest chars2))))))))))
+      (char-distance (string->list string1) (string->list string2))))
+  
+  (define (char-distance chars1 chars2)
+    (cond
+      ((null? chars1) 0) ; lists empty? we're done.  (Relies on lists of same length)
+      (else 
+       (let ((countDifferentChar (if (equal? (first chars1) (first chars2)) 0 1)))
+         (+ countDifferentChar (char-distance (rest chars1) (rest chars2)))))))
   
 (provide distance)
