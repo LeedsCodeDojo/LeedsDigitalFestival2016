d83ac1d32ba4ad867c21811a42775db47fdc4528
Grant Crofton <grantac@hotmail.com>
2016-04-27 15:18:24 +0100

    Added F# Hamming solution

---
+++ solutions/grant_fsharp/hamming.fsx
@@ -0,0 +1,13 @@
+﻿
+let hammingDistance string1 string2 = 
+    Seq.zip string1 string2
+    |> Seq.filter (fun (char1, char2) -> not (char1 = char2))
+    |> Seq.length
+
+match hammingDistance "abc123" "abc123" with
+    | 0 -> "Same strings PASSED"
+    | _ -> "Same strings FAILED"
+
+match hammingDistance "abc1234" "xbx1x3x" with
+    | 4 -> "Different strings PASSED"
+    | _ -> "Different strings FAILED"
